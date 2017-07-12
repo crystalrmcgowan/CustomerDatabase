20fe158595fe7f84fde2b95c7cb59d2eb5e46e12
Crystal <crystal.r.mcgowan@gmail.com>
2017-07-12 18:49:48 -0400

    Added Border to Name

--- main.js
+++ main.js
@@ -7,29 +7,29 @@ console.log(customers)
 //
 //   return _grabInfo
 // }
+// let _lessinfo = {
+//   picture: _customer.picture,
+//   name: _customer.name,
+//   email: _customer.email,
+//   location: _customer.location,
+//   phone: _customer.phone,
+//   id: _customer.id.value
+// }
 
 const list = document.querySelector("#directory")
 
 for (var i = 0; i < customers.results.length; i++) {
   let _customer = customers.results[i]
-  // let _lessinfo = {
-  //   picture: _customer.picture,
-  //   name: _customer.name,
-  //   email: _customer.email,
-  //   location: _customer.location,
-  //   phone: _customer.phone,
-  //   id: _customer.id.value
-  // }
 
   const item = document.createElement("div")
 
   item.innerHTML = `
     <img src= ${_customer.picture.large}>
-    <h3>${_customer.name.first} ${_customer.name.last}</h3>
+    <h3><span>${_customer.name.first} ${_customer.name.last}</span></h3>
     <p>${_customer.email}</p>
     <p>${_customer.location.street}</p>
-    <p>${_customer.location.city}, ${_customer.location.state} ${_customer
-    .location.postcode}</p>
+    <p>${_customer.location.city}, ${_customer.location.state}
+    ${_customer.location.postcode}</p>
     <p>${_customer.phone}</p>
     <p>${_customer.id.value}</p>
   `
